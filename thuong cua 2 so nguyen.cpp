7d4c5649e9fcdce540766b73f36c4ab60f5a69a9
Ngquy03 <nguyenquy29122003@gmail.com>
2023-08-15 00:05:11 +0700

    thuong cua 2 so nguyen

---
+++ thuong cua 2 so nguyen.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	float a,b;
+	printf("a:");
+	scanf("%f",&a);
+	printf("b:");
+	scanf("%f",&b);
+	float c;
+	c=a/b;
+	printf("thuong cua a va b la:%.1f",c);
+	return 0;
+}
