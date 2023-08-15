b1e3280a3738dcccf16ca57aa278e8cf3f8efb8b
Ngquy03 <nguyenquy29122003@gmail.com>
2023-08-16 01:00:39 +0700

    chu vi hinh tron

---
+++ chu vi hinh tron.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	float a,b;
+	printf("ban kinh hcn la:");
+	scanf("%f",&a);
+	printf("so pi la:");
+	scanf("%f",&b)
+	float c;
+	c=2*a*b;
+	printf("chu vi hinh tron la:%.1f",c);
+	return 0;
+}
