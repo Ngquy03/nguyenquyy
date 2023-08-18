9408a6d8822c67453d8b7d29f4e74c508ecc52b4
Ngquy03 <nguyenquy29122003@gmail.com>
2023-08-18 19:24:29 +0700

    tính điểm trung bình môn

---
+++ tinh diem trung binh mon.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	float a,b,c;
+ printf("diem toan la:");
+ scanf("%f",&a);
+ printf("diem ly la:");
+ scanf("%f",&b);
+ printf("diem hoa la:");
+ scanf("%f",&c);
+ float d;
+ d=((a*3)+(b*2)+c)/6;
+ printf("diem trung binh la:%f",d);
+ return 0;
+}
+
