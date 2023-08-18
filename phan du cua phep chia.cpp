e1f2ff2719ba36e474800e4fe762a2ab0d3456c5
Ngquy03 <nguyenquy29122003@gmail.com>
2023-08-18 15:44:59 +0700

    phan du cua phep chia

---
+++ phan du cua phep chia.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ int main(){
+ 	float a,b;
+ 	printf("a:");
+ 	scanf("%f",&a);
+ 	printf("b:");
+ 	scanf("%f",&b);
+ 	float c;
+ 	c=a%b;
+ 	printf("phan du cua phep chia la:");
+ 	return 0;
+ }
